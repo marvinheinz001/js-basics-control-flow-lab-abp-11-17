f8857f9eeabaa5871628202a374f509645bd199b
Max han <mwellh84@gmail.com>
2017-11-09 23:54:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ function ternaryCheckCity(city){
     return 'No go.'
   }
 }
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous'
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye'
+  }
+}
